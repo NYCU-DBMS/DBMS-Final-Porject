59d41780edf0dd3624003f7554fff9b2e44e61ca
bryan931218 <bryan931218@gmail.com>
2024-11-19 03:37:34 +0800

    Add profile and change password function

--- Frontend/src/components/Links.tsx
+++ Frontend/src/components/Links.tsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
 
 export default function NavBar() {
-	return (
-		<div className='dark:bg-slate-700'>
-			<nav>
-				<div className='flex gap-10 justify-center items-center border-b-[1px] h-14'>
-					<Link to="/">Home</Link>
-					<Link to="/favorites">Favorites</Link>
-				</div>
-			</nav>
-		</div>
-	)
+  return (
+    <div className='bg-[#1b2434]'>
+      <nav>
+        <div className='flex gap-10 justify-center items-center border-b-[1px] border-gray-600 h-14'>
+          <Link to="/" className="text-white hover:text-blue-300 transition-colors">Home</Link>
+          <Link to="/favorites" className="text-white hover:text-blue-300 transition-colors">Favorites</Link>
+        </div>
+      </nav>
+    </div>
+  )
 }
