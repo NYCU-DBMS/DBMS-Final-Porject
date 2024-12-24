cfbbd811dc740ddbddcf23ec1be65a99089b9353
youzhe0305 <money110209@gmail.com>
2024-12-25 03:28:39 +0800

    favorite & search error modify

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,7 @@ import { query } from './db';  // 移除 .js
 import db_test from './db_test';  // 移除 .js
 import db_init from './db_init';  // 移除 .js
 import search from './search';
+import favorite from './favorite';
 import { authRoutes } from './routes/authRoutes';  // 移除 .js 並使用具名導入
 import { animeRoutes } from './routes/animeRoutes';  //anime api
 import { categoryRoutes } from './routes/categoryRoutes'; //category api
@@ -42,7 +43,8 @@ app.use('/dbtest', db_test);
 app.use('/dbinit', db_init);
 app.use('/api/anime', animeRoutes);
 app.use('/api/category', categoryRoutes);
-app.use('/api/search', search)
+app.use('/api/search', search);
+app.use('/api/favorite', favorite);
 
 app.listen(port, () => {
   console.log(`Now listening port ${port}`);
