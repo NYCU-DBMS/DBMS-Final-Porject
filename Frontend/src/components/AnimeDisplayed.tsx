9f5e2bef005a6743c4f22ef3febb5bf24945cfdb
Hans <hansliao2015@gmail.com>
2024-12-25 20:24:57 +0800

    refactor: rename SearchBar to AnimeDisplayed and simplify functionality

--- Frontend/src/components/AnimeDisplayed.tsx
+++ Frontend/src/components/AnimeDisplayed.tsx
@@ -4,22 +4,10 @@ import { Input } from "@/components/ui/input"
 import AnimeGallery from "./AnimeGallery"
 import { fetchAnimeBySort } from "@/fetchAPI/fetchAnime"
 
-// pass data as props
-type SearchBarProps = {
-  data: string[]
-}
 
-export default function SearchBar({ data }: SearchBarProps) {
-  const [searchTerm, setSearchTerm] = useState("")
-  const [results, setResults] = useState(data)
+export default function AnimeDisplayed() {
   const [sort, setSort] = useState("score_asc")
   const [animeIds, setAnimeIds] = useState<number[]>([])
-
-  const handleSearch = (e: any) => {
-    const value = e.target.value
-    setSearchTerm(value)
-    setResults(data.filter((item) => item.toLowerCase().includes(value.toLowerCase())))
-  }
 
   useEffect(() => {
     const fetchAnimeIds = async () => {
@@ -34,8 +22,6 @@ export default function SearchBar({ data }: SearchBarProps) {
         <Input
           type="text"
           placeholder="Search anime..."
-          value={searchTerm}
-          onChange={handleSearch}
           className="w-full"
         />
         <select name="sort" id="sort" className="dark:bg-gray-800" onChange={(e) => setSort(e.target.value)}>
@@ -45,12 +31,6 @@ export default function SearchBar({ data }: SearchBarProps) {
           <option value="year_desc">year_desc</option>
         </select>
       </div>
-      {/* <div>
-        {results.length === 0 && searchTerm.length > 0 && <div>No results found</div>}
-        {results.map((item) => (
-          <div key={item}>{item}</div>
-        ))}
-      </div> */}
       <AnimeGallery animeIds={animeIds} />
     </div>
   )
