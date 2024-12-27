c5cddd8efd89c4d174b13eed545c6236ed8637a4
bryan931218 <bryan931218@gmail.com>
2024-12-27 17:52:12 +0800

    Fixed token problem and optimized FavoritePage UI

--- Frontend/src/pages/FavoriteListPage.tsx
+++ Frontend/src/pages/FavoriteListPage.tsx
@@ -4,7 +4,6 @@ import FavoriteList from "@/components/FavoriteList";
 export default function FavoriteListPage() {
   return (
     <div className="flex flex-col items-center my-10">
-      <div className="text-2xl font-bold">My Favorite Anime List</div>
       <FavoriteList />
     </div>
   )
